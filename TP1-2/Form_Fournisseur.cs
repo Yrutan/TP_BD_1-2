9fc3eaf6769f36f5413a08aa6b39366cd5f4a26b
Yrutan <201086438@P104-18.clg.priv>
2015-10-01 10:41:04 -0400

    add forms and a class

--- TP1-2/Form_Fournisseur.cs
+++ TP1-2/Form_Fournisseur.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TP1_2
 {
-    public partial class Form1 : Form
+    public partial class Form_Fournisseur : Form
     {
-        public Form1()
+        public Form_Fournisseur()
         {
             InitializeComponent();
         }
--- TP1-2/Program.cs
+++ TP1-2/Program.cs
@@ -16,7 +16,7 @@ namespace TP1_2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_Main());
         }
     }
 }
