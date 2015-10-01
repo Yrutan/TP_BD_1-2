e6a199ac1075e8dd3b2269fad1a96a3c1e5db7d9
Yrutan <201086438@P104-18.clg.priv>
2015-10-01 11:50:48 -0400

    lien dans le menu

--- TP1-2/Form_Main.Designer.cs
+++ TP1-2/Form_Main.Designer.cs
@@ -79,12 +79,14 @@
             this.LTSMI_InventaireAjout.Name = "LTSMI_InventaireAjout";
             this.LTSMI_InventaireAjout.Size = new System.Drawing.Size(152, 22);
             this.LTSMI_InventaireAjout.Text = "Ajout";
+            this.LTSMI_InventaireAjout.Click += new System.EventHandler(this.LTSMI_InventaireAjout_Click);
             // 
             // LTSMI_InventaireModification
             // 
             this.LTSMI_InventaireModification.Name = "LTSMI_InventaireModification";
             this.LTSMI_InventaireModification.Size = new System.Drawing.Size(152, 22);
             this.LTSMI_InventaireModification.Text = "Modification";
+            this.LTSMI_InventaireModification.Click += new System.EventHandler(this.LTSMI_InventaireModification_Click);
             // 
             // LTSMI_InventaireSupprimer
             // 
@@ -108,23 +110,26 @@
             this.LTSMI_FournisseurListe.Name = "LTSMI_FournisseurListe";
             this.LTSMI_FournisseurListe.Size = new System.Drawing.Size(152, 22);
             this.LTSMI_FournisseurListe.Text = "Liste";
+            this.LTSMI_FournisseurListe.Click += new System.EventHandler(this.LTSMI_FournisseurListe_Click);
             // 
             // LTSMI_FournisseurAjout
             // 
             this.LTSMI_FournisseurAjout.Name = "LTSMI_FournisseurAjout";
             this.LTSMI_FournisseurAjout.Size = new System.Drawing.Size(152, 22);
             this.LTSMI_FournisseurAjout.Text = "Ajout";
+            this.LTSMI_FournisseurAjout.Click += new System.EventHandler(this.LTSMI_FournisseurAjout_Click);
             // 
             // LTSMI_FournisseurModification
             // 
             this.LTSMI_FournisseurModification.Name = "LTSMI_FournisseurModification";
             this.LTSMI_FournisseurModification.Size = new System.Drawing.Size(152, 22);
             this.LTSMI_FournisseurModification.Text = "Modification";
+            this.LTSMI_FournisseurModification.Click += new System.EventHandler(this.LTSMI_FournisseurModification_Click);
             // 
             // LTSMI_FournisseurSupprimer
             // 
             this.LTSMI_FournisseurSupprimer.Name = "LTSMI_FournisseurSupprimer";
-            this.LTSMI_FournisseurSupprimer.Size = new System.Drawing.Size(152, 22);
+            this.LTSMI_FournisseurSupprimer.Size = new System.Drawing.Size(142, 22);
             this.LTSMI_FournisseurSupprimer.Text = "Supprimer";
             // 
             // rapportToolStripMenuItem
--- TP1-2/Form_Main.cs
+++ TP1-2/Form_Main.cs
@@ -19,7 +19,65 @@ namespace TP1_2
 
         private void LTSMI_InventaireListe_Click(object sender, EventArgs e)
         {
-            
+            Form_InventaireListe form = new Form_InventaireListe();
+
+            form.ShowDialog();
         }
+
+        private void LTSMI_InventaireAjout_Click(object sender, EventArgs e)
+        {
+            Form_InventaireAjout form = new Form_InventaireAjout();
+
+            if(form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //code pour ajout
+            }
+            else
+            {
+                // rien, l'opération est annulé
+            }
+
+        }
+
+        private void LTSMI_InventaireModification_Click(object sender, EventArgs e)
+        {
+            Form_InventaireModification form = new Form_InventaireModification();
+
+            if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //code pour update
+            }
+        }
+
+        private void LTSMI_FournisseurListe_Click(object sender, EventArgs e)
+        {
+            Form_FournisseurListe form = new Form_FournisseurListe();
+
+            form.ShowDialog();
+        }
+
+        private void LTSMI_FournisseurAjout_Click(object sender, EventArgs e)
+        {
+            Form_FournisseurAjout form = new Form_FournisseurAjout();
+
+            if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //code pour ajout
+            }
+        }
+
+        private void LTSMI_FournisseurModification_Click(object sender, EventArgs e)
+        {
+            Form_FournisseurModification form = new Form_FournisseurModification();
+
+            if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //code pour update
+            }
+        }
+
+
+
+
     }
 }
