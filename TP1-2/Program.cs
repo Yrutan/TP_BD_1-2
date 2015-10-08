24840e44112f3c5a46d76355ccdb3422e76ab1d8
Yrutan <simon.roy.26@hotmail.com>
2015-10-08 13:21:13 -0400

    ajout d'une variable connection
    on devrait pouvoir l'utiliser partout il faut juste mettre les bonnes
    variables. Évite de commit des changements à ce document sur git.

--- TP1-2/Program.cs
+++ TP1-2/Program.cs
@@ -3,17 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TP1_2
 {
     static class Program
     {
+        //l'objet connection utilisé partout dans l'application
+        //pourrait être modifié pour une fonction avec une valeur de retour qui retourne la connection
+        public static SqlConnection connection;
+
+
         /// <summary>
         /// Point d'entrée principal de l'application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            //les valeurs sont celles par défaut de l'exemple
+            // datasource est le chemin à la bd ?
+            String dataSource = @"PBRUNETD\\SQLEXPRESS";
+            // initial Catalog est le nom de la bd
+            String nameBD = "MaBD";
+            // user ID est le nom utiliser pour la connection à la bd
+            String userId = "maConnexion";
+            // password est le mot de passe du user ID pour se connecter à la bd
+            String pass = "connexion";
+            String chaineConnection = "Data Source="+dataSource+";Initial Catalog="+nameBD+";User ID="+ userId +";Password=" + pass;
+            connection = new SqlConnection(chaineConnection);
+            try
+            {
+                connection.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            // code de base pour un projet en c#
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form_Main());
